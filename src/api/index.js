ae9b732d800e3d2094fd7a71b43eb7699e1bb810
shijianing <shijianing1210@gmail.com>
2021-09-17 14:07:43 +0800

    add cloudbase

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,39 @@ export const fetchData = query => {
         params: query
     });
 };
+
+
+import cloudbase from "@cloudbase/js-sdk";
+console.log(cloudbase)
+const app = await cloudbase.init({
+  env: "dev-1gpp53ju3ceb46c7"
+});
+
+
+const auth = app.auth();
+async function login(){
+  await auth.anonymousAuthProvider().signIn();
+  // 匿名登录成功检测登录状态isAnonymous字段为true
+  const loginState = await auth.getLoginState();
+  console.log(loginState.isAnonymousAuth); // true
+}
+await login();
+
+// const db = await app.database();
+// const collection = await db.collection("User").doc('28ee4e3e604ed4b90aafd6d861995b34').get();
+// console.log(collection)
+// const res = await db.collection("User").doc('28ee4e3e604ed4b90aafd6d861995b34').update({ 'storeId':123});
+// console.log(res)
+
+app
+    .callFunction({
+      name: 'core',
+      data: {
+        apiName: 'user',
+        funcName: 'getByUnionId',
+        param: 'o-RM65odAK6kuYOhcNiag3hyD23Q'
+      }
+    })
+  .then((res) => {
+    console.log(res);
+  });
